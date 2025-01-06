a0b0e8c2cbc64c5433a7f59453adf7bfbe4ebcc7
DrBenson <Benson.Dr@GMail.com>
2025-01-06 10:05:55 +0800

    Update translation.

---
+++ Resources/translations/addFC_en.ts
@@ -0,0 +1,294 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en_US">
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../../InitGui.py" line="26"/>
+        <location filename="../../InitGui.py" line="47"/>
+        <location filename="../../InitGui.py" line="48"/>
+        <location filename="../../InitGui.py" line="69"/>
+        <source>addFC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../InitGui.py" line="27"/>
+        <source>addFC Workbench</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>addFC</name>
+    <message>
+        <location filename="../../addFC.py" line="67"/>
+        <source>Recent File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="70"/>
+        <source>Open recent file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="101"/>
+        <source>Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="104"/>
+        <source>Isometry and fit all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="126"/>
+        <source>Model Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="129"/>
+        <source>Run the model control file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="153"/>
+        <source>Model Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="156"/>
+        <source>Model information (BOM)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="404"/>
+        <source>Updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="420"/>
+        <source>Cleared</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="531"/>
+        <source>Elements are indexed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="542"/>
+        <source>Enumerations updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="553"/>
+        <source>Equations updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="782"/>
+        <source>Add Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="785"/>
+        <source>Add properties to an object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="921"/>
+        <source>You need to select an object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1152"/>
+        <source>Galvanized</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1214"/>
+        <source>Creating a Drawing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1217"/>
+        <source>Create a drawing based on a template</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1235"/>
+        <source>Select a template to create a drawing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1236"/>
+        <source>Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1320"/>
+        <location filename="../../addFC.py" line="1323"/>
+        <source>Help and Example</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1332"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1385"/>
+        <source>Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1388"/>
+        <source>Component library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1408"/>
+        <source>Explode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1411"/>
+        <source>Exploded view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1431"/>
+        <source>Pipe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC.py" line="1434"/>
+        <source>Creating a pipe by points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Info.py" line="135"/>
+        <source>Документация</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Info.py" line="505"/>
+        <location filename="../../addFC_Info.py" line="508"/>
+        <location filename="../../addFC_Info.py" line="682"/>
+        <location filename="../../addFC_Info.py" line="685"/>
+        <source>Прочие изделия</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Info.py" line="541"/>
+        <source>The BOM is empty...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Info.py" line="743"/>
+        <source>The allowed number of elements has been exceeded!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Info.py" line="910"/>
+        <source>Export complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="215"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="215"/>
+        <source>Addition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="215"/>
+        <source>Alias</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="215"/>
+        <location filename="../../addFC_Preference.py" line="278"/>
+        <location filename="../../addFC_Preference.py" line="421"/>
+        <location filename="../../addFC_Preference.py" line="523"/>
+        <location filename="../../addFC_Preference.py" line="608"/>
+        <location filename="../../addFC_Preference.py" line="687"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="216"/>
+        <source>Property</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="216"/>
+        <location filename="../../addFC_Preference.py" line="306"/>
+        <location filename="../../addFC_Preference.py" line="422"/>
+        <source>Values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="294"/>
+        <source>... use the materials tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="523"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="523"/>
+        <source>Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="523"/>
+        <source>Unit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="523"/>
+        <source>Price per unit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="764"/>
+        <source>Thickness</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="764"/>
+        <source>Radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Preference.py" line="764"/>
+        <source>K-Factor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Unfold.py" line="62"/>
+        <source>Warning: SheetMetal Workbench is not available!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Unfold.py" line="67"/>
+        <source>No sheet metal parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Unfold.py" line="191"/>
+        <source>wrong, let&apos;s try a spare face...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../addFC_Unfold.py" line="312"/>
+        <source>...done</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
